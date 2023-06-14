2f81e29e9b8d0eb4d98abf6616bf27040963db1e
Yasser Gribi <yasser.gribii@gmail.com>
2023-06-14 15:40:41 +0100

    Add items to admin panel

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title','price','seller','category_name','last_update']
+    search_fields = ['title']
+    prepopulated_fields = {'slug' : ['title']}
+    list_per_page = 10
+    #list_select_related = ['category']
+
+
+    def category_name(self, item):
+        return item.category.title
+
+
+admin.site.register(models.Item , ItemAdmin)
