413b5342f1bc3feb6a88a6c2aedf7ec3f6a00836
Yasser Gribi <yasser.gribii@gmail.com>
2023-06-28 14:22:35 +0100

    Adding custom filters for items endpoint

--- marketplace/views.py
+++ marketplace/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import get_object_or_404 , render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet, GenericViewSet, ViewSet
 from rest_framework.permissions import  DjangoModelPermissions, IsAuthenticated , IsAuthenticatedOrReadOnly, AllowAny, IsAdminUser
 
+from .filters import ItemFilter
+from .pagination import DefaultPagination
 from .permissions import IsAdminOrReadOnly, IsAdminOrOwnerOrReadOnly, IsAdminOrOwner
 from .models import Category, Favorite, Item, Profil
 from .serializers import AddItemSerializer, SimpleProfilSerializer, CreateProfilSerializer ,AddItemToFavSerializer ,CategorySerializer, FavoriteItemsSerializer, ItemSerializer, ProfilSerializer, UpdateItemSerializer
@@ -31,7 +35,7 @@ class AdminProfilViewSet(ModelViewSet):
     permission_classes = [IsAdminUser]
     
 
-    @action(detail=False, methods=['GET', 'PATCH', 'DELETE'], permission_classes = [IsAuthenticated])
+    @action(detail=False, methods=['GET', 'PATCH', 'DELETE'], permission_classes = [IsAdminUser])
     def me(self, request):
         #current_user = request.user
         profil = Profil.objects.get(user= request.user)
@@ -43,6 +47,8 @@ class AdminProfilViewSet(ModelViewSet):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data)
+        elif request.method == 'DELETE':
+            return Response({}, status.HTTP_204_NO_CONTENT)
 
 
 class ProfilViewSet(ModelViewSet):
@@ -85,22 +91,26 @@ class CategoryViewSet(ModelViewSet):
 
 
 class FavoriteViewSet(ModelViewSet):
+    http_method_names = ['get','post', 'delete']
     permission_classes = [IsAuthenticated]
+    
+    
 
-    @action(detail=False, methods=['GET', 'PUT'])
+    @action(detail=False, methods=['GET'])
     def get_queryset(self):
-        if self.request.user.is_staff and self.kwargs['profil_pk'] != 'me':
+        current_user = self.request.user
+        if current_user.is_staff and self.kwargs['profil_pk'] != 'me':
             return Favorite.objects.select_related('item__category', 'item__seller__user')\
-                        .filter(customer= self.kwargs['profil_pk']).all()
+                                   .filter(customer= self.kwargs['profil_pk']).all()
         else:
             return Favorite.objects.select_related('item__category', 'item__seller__user')\
-                            .filter(customer__user=self.request.user).all()
+                                   .filter(customer__user= current_user).all()
 
     def get_serializer_class(self):
-        if self.request.method == 'POST':
-            return AddItemToFavSerializer
+        if self.request.method == 'GET':
+            return FavoriteItemsSerializer 
         else:
-            return FavoriteItemsSerializer
+            return AddItemToFavSerializer
 
     # def get_serializer_context(self):
     #     return {'profil_id': self.kwargs['profil_pk']}
@@ -108,6 +118,11 @@ class FavoriteViewSet(ModelViewSet):
 class ItemViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
     permission_classes = [IsAdminOrOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ItemFilter
+    pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     queryset = Item.objects.select_related('seller__user','category').all()
 
@@ -118,7 +133,7 @@ class ItemViewSet(ModelViewSet):
             return UpdateItemSerializer
         return ItemSerializer
 
-class MyOffersViewSet(ModelViewSet):
+class OffersViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     def get_queryset(self):
         return  Item.objects.select_related('seller__user','category')\
